a108bf2b659b08b882bd5666845beff00a408525
Prakriti <nature.prakriti123@gmail.com>
2021-10-31 20:09:06 +0530

    added brainWiz event

--- data/data.js
+++ data/data.js
@@ -3,4 +3,5 @@ module.exports = [
     { name: "SciWay", image: "../images/sciway.png", registration: "https://forms.gle/tpQfgEKYb5Ft6Ba49", rulebook: "../rulebooks/sciway_rulebook.pdf" },
     { name: "Icon", image: "../images/icon.png", registration: "https://docs.google.com/forms/d/e/1FAIpQLScD6y0VOKigcBye0EWkMy3cO0kOSDUxFM41pb1C7eptedfKCQ/viewform?usp=sf_link", rulebook: "../rulebooks/icon_rulebook.pdf" },
     { name: "Rostruct", image: "../images/Rostruct.png", registration: " https://lnkd.in/eHf685pD", rulebook: "../rulebooks/Rostruct_rulebook.pdf" },
+    { name: "BrainWiz", image: "../images/brain_wiz.png", registration: "https://forms.gle/ezkNPS5shx93w4Qb7", rulebook: "../rulebooks/brainWiz_rulebook.pdf" },
 ]
